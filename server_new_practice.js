0933f45d735d97feb9ca357ad2d038d2416478e4
Romer Alexander Almeida <raalmeidab22@itp.edu.co>
2024-09-04 20:16:50 -0500

    carga_)?_server_new_practice.js

--- server_new_practice.js
+++ server_new_practice.js
@@ -56,7 +56,7 @@ const server = http.createServer((req, res) => {
   else {
     res.statusCode = 404;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Viejo... ¿Que pagina andas buscando? -.-');
+    res.end('Viejo... ¿Que pagina andas buscando? -.-)?');
   }
 });
 
